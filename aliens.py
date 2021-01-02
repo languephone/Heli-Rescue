c002ac50edf8b8c90b64a123bc2339c7af85e01f
languephone <languephone@users.noreply.github.com>
2021-01-02 12:13:36 +0000

    Added timer and created loop of x values to test alien creation
    Added timer to test length of execution and created a loop of x values to simulate traversal through game.  Loop prints 'creating an alien' whenever the x value matches the x value of an alien start.

--- aliens.py
+++ aliens.py
@@ -1,4 +1,4 @@
-import csv
+import csv, time
 
 filename = 'enemy_map.csv'
 with open(filename, encoding='utf-8-sig') as f:
@@ -14,3 +14,27 @@ with open(filename, encoding='utf-8-sig') as f:
 		aliens.append(alien)
 
 	print(aliens)
+
+	start_values = []
+	for alien in aliens:
+		start_values.append(int(alien['Start']))
+	print(start_values)
+
+t0 = time.time()
+
+max_x = max(start_values)
+x = 0
+while x <= max_x:
+	print(x)
+	for i in range(len(start_values.copy())):
+		if start_values[i] <= x:
+			print(f'Creating an alien with {aliens[i]} attributes')
+			start_values.remove(start_values[i])
+			break
+
+	x += 1
+
+t1 = time.time()
+
+total_time = t1 - t0
+print(total_time)
