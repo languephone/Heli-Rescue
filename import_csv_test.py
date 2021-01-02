4774445c60f47b498835b82f4cf6b20c3894484d
languephone <languephone@users.noreply.github.com>
2021-01-02 10:47:24 +0000

    Added file to manage importing alien pattern text file
    Created new file to handle opening and reading of csv file containing list of aliens.

---
+++ import_csv_test.py
@@ -0,0 +1,16 @@
+import csv
+
+filename = 'enemy_map.csv'
+with open(filename, encoding='utf-8-sig') as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+
+	# Get list of values from the file.
+	aliens = []
+	for row in reader:
+		alien = {}
+		for i in range(len(header_row)):
+			alien[header_row[i]] = row[i]
+			aliens.append(alien)
+
+	print(aliens)
