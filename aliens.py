fb4179578452b870fb0e74fc3dcca245ea3564b4
languephone <languephone@users.noreply.github.com>
2021-01-02 12:24:42 +0000

    Added list of aliens created to verify result
    Added new list of aliens created by loop to test that the correct aliens were created.  Also removed aliens from main list using pop() function as they were created.

--- aliens.py
+++ aliens.py
@@ -20,21 +20,31 @@ with open(filename, encoding='utf-8-sig') as f:
 		start_values.append(int(alien['Start']))
 	print(start_values)
 
+
+alien_test_list = []
+
+# Start timer------------
 t0 = time.time()
 
 max_x = max(start_values)
+
 x = 0
+
 while x <= max_x:
 	print(x)
-	for i in range(len(start_values.copy())):
-		if start_values[i] <= x:
-			print(f'Creating an alien with {aliens[i]} attributes')
-			start_values.remove(start_values[i])
-			break
+	if start_values[0] <= x:
+		for i in range(len(start_values.copy())):
+			if start_values[i] <= x:
+				print(f'Creating an alien with {aliens[i]} attributes')
+				alien_test_list.append(aliens.pop(i))
+				start_values.remove(start_values[i])
+				break
 
 	x += 1
 
+# End timer--------------
 t1 = time.time()
 
 total_time = t1 - t0
 print(total_time)
+print(alien_test_list)
